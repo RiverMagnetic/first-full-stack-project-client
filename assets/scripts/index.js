fc457feed41950d64b17141eb1d27e8d3b920ba0
RiverMagnetic <magneticcontinuum@gmail.com>
2018-07-21 21:31:24 -0400

    Add starter code

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,35 @@
 'use strict'
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
+// const game = require('./game')
+const authEvents = require('./auth/authEvents')
+// const gameEvents = require('./game/gameEvents')
 
-// use require without a reference to ensure a file is bundled
-// require('./example')
 
+
+// on document ready
 $(() => {
-  // your JS code goes here
+
+  // $('#game-board').hide()
+  // $('#change-password-modal').hide()
+  // $('#sign-out-modal').hide()
+  // $('#game-create').hide()
+
+  // function to hold event handlers
+  // const addHandlers = () => {
+  //   $('.cell').on('click', function (event) {
+  //     event.preventDefault()
+  //     game.mark(event.target.id)
+  //   })
+  //   $('#game-create').on('submit', function (event) {
+  //     event.preventDefault()
+  //     game.createGame(event)
+  //   })
+  // }
+  // call the addHandlers function that was just defined
+  // addHandlers()
+
+  authEvents.addHandlers()
+  // gameEvents.addHandlers()
+  $('#sign-up').on('submit', authEvents.openModal)
+  // $('#change-password').on('submit', authEvents.openModal)
 })
