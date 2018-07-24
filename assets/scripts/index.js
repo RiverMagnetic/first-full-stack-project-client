9c3764286a19f57d8bc75c9f6d2a13fbdb275a67
RiverMagnetic <magneticcontinuum@gmail.com>
2018-07-23 20:12:53 -0400

    Remove code from past project

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,5 @@
 'use strict'
 
-// const game = require('./game')
 const authEvents = require('./auth/authEvents')
 const itemEvents = require('./item/itemEvents')
 
@@ -9,20 +8,14 @@ const itemEvents = require('./item/itemEvents')
 // on document ready
 $(() => {
 
-  // $('#game-board').hide()
+  $('#items-view').hide()
   $('#change-password-modal').hide()
   $('#sign-out-modal').hide()
-  $('#list-view').hide()
-
+  
   // function to hold event handlers
   const addHandlers = () => {
-  //   $('.cell').on('click', function (event) {
-  //     event.preventDefault()
-  //     game.mark(event.target.id)
-  //   })
     $('#list-view').on('submit', function (event) {
       event.preventDefault()
-      // game.createGame(event)
     })
   }
   // call the addHandlers function that was just defined
