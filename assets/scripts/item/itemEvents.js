8b9bca1ac3e86c761aee29e93405e2e6bcc3a115
RiverMagnetic <magneticcontinuum@gmail.com>
2018-08-01 20:28:37 -0400

    Clean up code

--- assets/scripts/item/itemEvents.js
+++ assets/scripts/item/itemEvents.js
@@ -31,16 +31,8 @@ const onDeleteItem = (event) => {
 const onUpdateItem = function (event) {
     event.preventDefault()
     console.log('running onUpdateItem')
-
     const data = getFormFields(event.target)
     const item = data.item
-
-    console.log(item)
-    // if (item.id === '') {
-    //     $('#message').html('Id is required')
-    //     $('#message').css('background - color', 'red')
-    //     return false
-    // }
     if (item.id.length !== 0) {
         // console.log(item.id.length)
         // console.log(data)
